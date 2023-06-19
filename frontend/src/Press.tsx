524616db83a9e6e095949c72789935ffe2a6b365
Austin Williams <austinwilliams@college.harvard.edu>
2023-06-19 09:56:20 -0400

    more press links

--- frontend/src/Press.tsx
+++ frontend/src/Press.tsx
@@ -15,6 +15,32 @@ export const Press = () => {
                         Costco
                     </Button>
                 </Grid>
+                {/* https://www.kittenaroundcatlounge.com/ */}
+                <Grid item xs={12} textAlign={"center"}>
+                    <Button
+                        variant="text"
+                        onClick={() => window.open("https://www.kittenaroundcatlounge.com/", "_blank", "noreferrer")}
+                    >
+                        Kitten Around
+                    </Button>
+                </Grid>
+                {/* https://www.amazon.com/dp/B0BD56NC8R/ref=twister_B09TW93ZFP?_encoding=UTF8&psc=1 */}
+                <Grid item xs={12} textAlign={"center"}>
+                    <Button
+                        variant="text"
+                        onClick={() => window.open("https://www.amazon.com/Underwear-Stretch-Underpants-Breathable-Printed/dp/B0BWD6L7ZH", "_blank", "noreferrer")}
+                    >
+                        Underpants
+                    </Button>
+                </Grid>
+                <Grid item xs={12} textAlign={"center"}>
+                    <Button
+                        variant="text"
+                        onClick={() => window.open("https://www.amazon.com/dp/B0BD56NC8R/ref=twister_B09TW93ZFP?_encoding=UTF8&psc=1", "_blank", "noreferrer")}
+                    >
+                        Overpants
+                    </Button>
+                </Grid>
             </Grid>
         </Stack>
     )
